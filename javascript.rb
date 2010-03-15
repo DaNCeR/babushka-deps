db30a51b85236b56f162a18e9878d707d3189184
Nicholas Dancer <dancer@feistystudios.com>
2010-03-16 03:49:40 +1000

    Added a dep for node.js

--- javascript.rb
+++ javascript.rb
@@ -1,3 +1,10 @@
 tmbundle 'Javascript Tools.tmbundle' do
   source 'git://github.com/johnmuhl/javascript-tools-tmbundle.git'
 end
+
+src 'node' do
+  source "http://nodejs.org/dist/node-v0.1.32.tar.gz"
+  configure { shell default_configure_command }
+  build { shell "make" }
+  install { shell 'make install', :sudo => true }
+end
